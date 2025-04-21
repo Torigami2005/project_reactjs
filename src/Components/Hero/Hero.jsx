f4a96ab4440d0c9e47456ba675e4c730459c552e
Eman Torr Berjame <emantorrberjame@gmail.com>
2025-04-21 19:27:43 +0800

    hero component changed to bg video instead of hero photo

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './Hero.css'
 import { FaArrowRightLong } from "react-icons/fa6";
+import video_loop from '../../assets/Echoes_LED.mp4'
 
 const Hero = () => {
   return (
     <div className='hero container'>
+      <video className='hero-video' autoPlay loop muted>
+        <source src={video_loop} type="video/mp4"/>
+      </video>
       <div className="hero-text">
         <h2>ᴇᴄʜᴏᴇꜱ ᴏꜰ ᴛʜᴇ ᴘᴀꜱᴛ, ᴠɪʙʀᴀᴛɪᴏɴꜱ ᴏꜰ ᴛʜᴇ ꜰᴜᴛᴜʀᴇ</h2>
         <p>Echoes is a group band and a subcommittee of the AdDU CS Talents.
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -34,9 +34,7 @@ const Navbar = () => {
 
   return (
     <nav className={`container ${sticky ? 'dark-nav' : ''}`}>  
-    <Link to='hero' smooth={true} offset={0} duration={500}>
         <img src={logo} alt="" className='logo' />
-        </Link>
         <ul className={mobileMenu ?'': 'hide-mobile-menu'}>
             <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
             <li><Link to='programs' smooth={true} offset={-260} duration={500}>Programs</Link></li>
